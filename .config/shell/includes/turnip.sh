286fc8ef11c2fd36155ab7ad85e6ee9e0cd96c2f
ウエスリー <wesley@cipolla.evokateur.net>
2025-08-17 17:00:11 -0700

    remind me when I am not on turnip

--- .config/shell/includes/turnip.sh
+++ .config/shell/includes/turnip.sh
@@ -1,34 +1,43 @@
 plait() {
+    echo "[turnip]"
     ssh turnip.local "/Users/wesley/bin/plait $(printf '%q ' "$@")"
 }
 
 plaiting() {
+    echo "[turnip]"
     ssh turnip.local "/Users/wesley/bin/plaiting"
 }
 
 unplait() {
+    echo "[turnip]"
     ssh turnip.local "/Users/wesley/bin/unplait"
 }
 
 drummer() {
+    echo "[turnip]"
     ssh turnip.local "/Users/wesley/bin/drummer"
 }
 
 kalx() {
+    echo "[turnip]"
     ssh turnip.local "/Users/wesley/bin/kalx"
 }
 kdvs() {
+    echo "[turnip]"
     ssh turnip.local "/Users/wesley/bin/kdvs"
 }
 
 kxlu() {
+    echo "[turnip]"
     ssh turnip.local "/Users/wesley/bin/kxlu"
 }
 
 rfi() {
+    echo "[turnip]"
     ssh turnip.local "/Users/wesley/bin/rfi"
 }
 
 wfmu() {
+    echo "[turnip]"
     ssh turnip.local "/Users/wesley/bin/wfmu"
 }
