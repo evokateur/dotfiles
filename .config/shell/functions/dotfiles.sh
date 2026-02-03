5243f4b6afe5071541a54722b8b4f7dabdcaa257
ウエスリー <wesley@cipolla.evokateur.net>
2026-02-03 14:44:59 -0800

    more informative dotfiles 'git add .' refusal

--- .config/shell/functions/dotfiles.sh
+++ .config/shell/functions/dotfiles.sh
@@ -1,7 +1,9 @@
 dotfiles() {
     if [[ "$1" == "add" && "$2" == "." ]]; then
-        echo "❌ Refusing to run 'dotfiles add .' — be specific!"
+        echo "Refusing to add everything in your home directory."
+        echo "You probably meant to run 'dotfiles add -u'.."
+        # 'dotfiles add -u' will add only tracked files that are modified or deleted.
         return 1
     fi
-    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME "$@"
+    /usr/bin/git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" "$@"
 }
