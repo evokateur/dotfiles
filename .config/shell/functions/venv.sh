ba013e5a888bdc78659e4b50be26848e585bb258
ウエスリー <wesley@cipolla.evokateur.net>
2026-02-10 23:43:16 -0800

    add nvim wrapper that runs with uv if uv.lock exists in cwd or above

--- .config/shell/functions/venv.sh
+++ .config/shell/functions/venv.sh
@@ -41,3 +41,21 @@ dump_venv() {
     echo "VENV_ROOT=$VENV_ROOT"
     echo "VIRTUAL_ENV=$VIRTUAL_ENV"
 }
+
+nvim() {
+    search_dir="$PWD"
+    found_uv_lock=""
+    while [ "$search_dir" != "/" ]; do
+        if [ -f "$search_dir/uv.lock" ]; then
+            found_uv_lock="$search_dir/uv.lock"
+            break
+        fi
+        search_dir="$(dirname "$search_dir")"
+    done
+
+    if [ -n "$found_uv_lock" ]; then
+        uv run nvim "$@"
+    else
+        command nvim "$@"
+    fi
+}
