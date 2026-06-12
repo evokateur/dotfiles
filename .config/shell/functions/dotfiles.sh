c993378ca5a7a13dc9d3ee20daf2e775128baa6a
ウエスリー <wesley@cipolla.evokateur.net>
2026-06-12 06:57:23 -0700

    ok STET

--- .config/shell/functions/dotfiles.sh
+++ .config/shell/functions/dotfiles.sh
@@ -2,12 +2,12 @@ dotfiles() {
     if [[ "$1" == "add" ]]; then
         case "$2" in
         .)
-            echo "That would add every untracked file in '$PWD' D:"
+            echo "That would add every untracked file in ${PWD}! D:"
             echo "You probably meant: 'dotfiles add -u'"
             return 1
             ;;
         -A | --all)
-            echo "That would add every untracked file in '$HOME' D:"
+            echo "That would add every untracked file in ${HOME}! D:"
             echo "You probably meant: 'dotfiles add -u'"
             return 1
             ;;
