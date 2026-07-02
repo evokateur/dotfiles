4a40e19db17cb541c3a4121085f6fe7d279eadcb
ウエスリー <wesley@sfizio.evokateur.net>
2026-07-02 13:05:02 -0700

    pull more src in pull-env

--- .bin/helpers/fancy-echo.sh
+++ .bin/helpers/fancy-echo.sh
@@ -1,9 +1,15 @@
 . "$HOME/.config/shell/env/colors.sh"
 
-PREFIX="${ITALIC}${BLUE}"
 SUFFIX="${RESET}"
 
 blue_italic_echo() {
+    PREFIX="${ITALIC}${BLUE}"
+    local m="$*"
+    printf '%b\n' "${PREFIX}${m}${SUFFIX}"
+}
+
+red_echo() {
+    PREFIX="${BOLD}${RED}"
     local m="$*"
     printf '%b\n' "${PREFIX}${m}${SUFFIX}"
 }
